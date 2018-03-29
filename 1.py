4a4c3547035e0c387150c375c0e048bd79bd339f
Rostikst <delfin4uk@gmail.com>
2018-03-29 22:16:06 +0300

    Add 1.py

---
+++ 1.py
@@ -0,0 +1,6 @@
+name = raw_input("What is your name?\n")
+age  = raw_input("How old are you?\n")
+place_of_living = raw_input("Where are you live?\n")
+print "Hello " + name + "!",
+print "Your age is " + age + "!",
+print "Your live in " + place_of_living + "!"
