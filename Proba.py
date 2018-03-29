32f009a6f2e4535b39f3caee70ce3f0cc6f7230f
Rostikst <delfin4uk@gmail.com>
2018-03-29 22:19:38 +0300

    Add Proba.py

--- Proba.py
+++
@@ -1,6 +0,0 @@
-name = raw_input("What is your name?\n")
-age  = raw_input("How old are you?\n")
-place_of_living = raw_input("Where are you live?\n")
-print "Hello " + name + "!",
-print "Your age is " + age + "!",
-print "Your live in " + place_of_living + "!"
