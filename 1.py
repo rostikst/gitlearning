58b8d92df9f317fe8f61bd39ce908230b5e7c305
rostikst <delfin4uk@gmail.com>
2018-03-30 13:55:43 +0300

    add 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 name = raw_input("What is your name?\n")
 age  = raw_input("How old are you?\n")
 place_of_living = raw_input("Where are you live?\n")
-print "Hello " + name + "!",
-print "Your age is " + age + "!",
-print "You live in " + place_of_living + "!"
+print "Hello " + name + "!"
+print "Your age is " + age + " and",
+print "you live in " + place_of_living + "!"
--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
+from math import pow
 
-
-a = raw_input("Enter value a\n")
-b = raw_input("Enter value b\n")
+a = raw_input("Enter value a:\n")
+b = raw_input("Enter value b:\n")
 x = pow(int(a), int(b))
 print x
